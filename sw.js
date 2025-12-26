ee894f0ef88953801630851b9f809bc9e7c6dcda
mahitmass <mahit.anuragagarwal@gmail.com>
2025-12-26 23:32:22 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,34 +1,38 @@
-const CACHE_NAME = "tt-cache-v1";
-// List all files you want to work offline (HTML, CSS, JS, Images)
+const CACHE_NAME = "tt-cache-v2";
 const ASSETS_TO_CACHE = [
-  "/",
-  "/index.html",
-  "/style.css",
-  "/script.js",
-  "/timetable_data.json" 
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./Logo.png", 
+  "./icon.png"
 ];
 
-// 1. Install Event: Caches the files when the site is first visited
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      console.log("Caching assets...");
       return cache.addAll(ASSETS_TO_CACHE);
     })
   );
 });
 
-// 2. Fetch Event: The "Offline First" Logic
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    // Check if the requested file is in the cache
     caches.match(event.request).then((cachedResponse) => {
-      // If found in cache, return it (No Internet Needed!)
-      if (cachedResponse) {
-        return cachedResponse;
-      }
-      // If not in cache, try to get it from the internet
-      return fetch(event.request);
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
     })
   );
 });
